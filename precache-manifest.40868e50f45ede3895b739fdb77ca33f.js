cd925d68c0065bbba7207ee670c91df438f31a45
stayacid <stayacid@gmail.com>
2020-06-03 18:41:16 +0300

    Updates

---
+++ precache-manifest.40868e50f45ede3895b739fdb77ca33f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "407dd37e57173af58020d1012a3218eb",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "0fdae1f3a5bbbc5ab198",
+    "url": "/react-todo/static/css/main.d6a8ef96.chunk.css"
+  },
+  {
+    "revision": "01e6ffd148a88a0351a9",
+    "url": "/react-todo/static/js/2.efac78bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.efac78bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00e262a4480890ba412e",
+    "url": "/react-todo/static/js/3.a24a24e8.chunk.js"
+  },
+  {
+    "revision": "0fdae1f3a5bbbc5ab198",
+    "url": "/react-todo/static/js/main.dce62098.chunk.js"
+  },
+  {
+    "revision": "02877d106556c297fe35",
+    "url": "/react-todo/static/js/runtime-main.130eb940.js"
+  }
+]);
